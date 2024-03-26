fd662656a7f1a4f500a70892262ea4bac6185a76
6uohul <kiy010604@naver.com>
2024-03-26 18:29:12 +0900

    [LV1]가장 많이 받은 선물

---
+++ Lv1/가장많이받은선물.py
@@ -0,0 +1,10 @@
+def solution(friends, gifts):
+    friendNum = friends.len
+
+    answer = 0
+    return answer
+
+
+
+solution(["muzi", "ryan", "frodo", "neo"],
+         ["muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"])
