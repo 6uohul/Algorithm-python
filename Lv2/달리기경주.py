0d1136b333a08548c9738583d8cbff9b9175bdac
6uohul <kiy010604@naver.com>
2024-03-27 13:06:50 +0900

    [Lv2]달리기경주_solved

--- Lv2/달리기경주.py
+++ Lv2/달리기경주.py
@@ -1,9 +1,11 @@
 def solution(players, callings):
-    for c in callings:
-        index = players.index(c)
-        players[index], players[index - 1] = players[index - 1], players[index]
+    racing_dic = {player: index for index, player in enumerate(players)}
+    for calling in callings:
+        current_position = racing_dic[calling]
+        player_to_swap = players[current_position - 1]
 
-    print(players)
+        players[current_position], players[current_position-1] = players[current_position-1], players[current_position]
+        racing_dic[calling], racing_dic[player_to_swap] = current_position - 1, current_position
     return players
 
 solution(["a","b","c"], ["b", "c", "c"])
