04b3f5a836a6a4cfa67cfa0d5b7c9e5f748041d8
6uohul <kiy010604@naver.com>
2024-03-27 16:13:08 +0900

    [Lv2]최댓값과 최솟값-코드수정

--- Lv2/최댓값과_최솟값.py
+++ Lv2/최댓값과_최솟값.py
@@ -1,17 +1,5 @@
 def solution(s):
     intArray = list(map(int, s.split()))  # 문자열에서 정수 리스트로 변환
-
-    # 초기 최솟값과 최댓값을 설정합니다. 첫 번째 요소로 초기화합니다.
-    minValue = maxValue = intArray[0]
-
-    # 리스트를 순회하면서 최솟값과 최댓값을 찾음
-    for num in intArray[1:]:
-        if num < minValue:
-            minValue = num
-        elif num > maxValue:
-            maxValue = num
-
-    answer = str(minValue) + " " + str(maxValue)
-    print(answer)
+    answer = str(min(intArray)) + " " + str(max(intArray))
 
 solution("1 2 3 -4")
