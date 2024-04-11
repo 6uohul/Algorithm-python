a5d04285068c4e4c50cdb780db292e3d7a94218f
6uohul <kiy010604@naver.com>
2024-04-11 19:35:59 +0900

    [Lv1]k번째 수_for문 수정

--- Lv1/k_번째_수.py
+++ Lv1/k_번째_수.py
@@ -1,13 +1,9 @@
 def solution(array, commands):
-    answer = []
-    for i in range(0, len(commands)):
-        start_index = commands[i][0]
-        end_index = commands[i][1]
-        select_index = commands[i][2]
-        new_array = array[start_index-1:end_index]
-        new_array = sorted(new_array)
-        answer.append(new_array[select_index-1])
-    return answer
+    result = []
+    for command in commands:
+        start, end, select = command
+        result.append(sorted(array[start - 1:end])[select - 1])
+    return result
 
 
 solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]])
