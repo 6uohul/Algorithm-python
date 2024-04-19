1f94043f87e93bcf1ef54a6b88e4dc2122cb6d4b
6uohul <kiy010604@naver.com>
2024-04-19 22:57:15 +0900

    [Lv1]모의고사

---
+++ Lv1/모의고사.py
@@ -0,0 +1,21 @@
+def solution(answers):
+    n = len(answers)
+    score = {1: 0, 2: 0, 3: 0}
+    student1 = [1,2,3,4,5] * ((n // 5) + 1)
+    student2 = [2,1,2,3,2,4,2,5] * ((n // 8) + 1)
+    student3 = [3,3,1,1,2,2,4,4,5,5] * ((n // 10) + 1)
+
+    for num in range(0,n):
+        if answers[num] == student1[num]:
+            score[1] += 1
+        if answers[num] == student2[num]:
+            score[2] += 1
+        if answers[num] == student3[num]:
+            score[3] += 1
+            
+    max_score = max(score.values())
+    result = [student for student, student_score in score.items() if student_score == max_score]
+
+    return result
+
+solution([1,3,2,4,2])
