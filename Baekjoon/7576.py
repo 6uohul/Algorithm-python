5a0093867c016d86c44f64fe0ffe72c4cc69ccfb
6uohul <kiy010604@naver.com>
2025-01-28 17:16:52 +0900

    [Python] 7576 토마토 - BFS

---
+++ Baekjoon/7576.py
@@ -0,0 +1,38 @@
+### 토마토 BFS
+
+from collections import deque
+
+M, N = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(n)]
+q = deque()
+
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 1:
+            q.append([i, j])
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
+while q:
+    x, y = q.popleft()
+    for i in range(4):
+        # 익은 토마토 상하좌우 돌면서 일수 저장하기
+        nx = x + dx[i]
+        ny = y + dy[i]
+    
+        if 0 <= nx < N and 0 <= ny < M:
+            if graph[nx][ny] == 0:
+                graph[nx][ny] =  graph[x][y] + 1
+                q.append([nx, ny])
+
+ans = 0
+for line in graph:
+    for tomato in line:
+        if tomato == 0:
+            print(-1)
+            exit()
+        
+    ans = max(ans, max(line))
+
+print(ans - 1)
