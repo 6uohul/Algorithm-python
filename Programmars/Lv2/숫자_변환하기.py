2c8990a0965af823e77b145135231d7d9e616019
6uohul <kiy010604@naver.com>
2025-03-20 15:02:25 +0900

    [Python] DP - 숫자 변환하기

---
+++ Programmars/Lv2/숫자_변환하기.py
@@ -0,0 +1,35 @@
+'''
+문제 설명
+자연수 x를 y로 변환하려고 합니다. 사용할 수 있는 연산은 다음과 같습니다.
+
+x에 n을 더합니다
+x에 2를 곱합니다.
+x에 3을 곱합니다.
+자연수 x, y, n이 매개변수로 주어질 때, x를 y로 변환하기 위해 필요한 최소 연산 횟수를 return하도록 solution 함수를 완성해주세요.
+이때 x를 y로 만들 수 없다면 -1을 return 해주세요.
+'''
+
+def solution(x, y, n):
+    m = [10e9] * (y+1)
+    m[x] = 0
+
+    for i in range(x, y+1):
+        # m[x] = 방문한적없는 경우 10e9인 경우엔 계속하기
+        if m[x] == 10e9:
+            continue
+        # i+5 <= y 인경우
+        if i+n <= y:
+            m[i+n] = min(m[i+n], m[i]+1)
+        # i*2 <= y
+        if i*2 <= y:
+            m[i*2] = min(m[i*2], m[i]+1)
+        # i*3 <= y
+        if i*3 <= y:
+            m[i*3] = min(m[i*3], m[i]+1)
+        # m[y] == 10e9인경우
+    if m[y] == 10e9:
+        m[y] = -1
+
+    return m[y]
+
+print(solution(2,5,4))
