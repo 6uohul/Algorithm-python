728207a43c3f13dbe4172975511a37b515347e2c
6uohul <kiy010604@naver.com>
2025-03-24 17:43:27 +0900

    [Python] 게임 맵 최단거리 수정

--- Programmars/Lv2/게임_맵_최단거리.py
+++ Programmars/Lv2/게임_맵_최단거리.py
@@ -17,32 +17,30 @@ maps는 0과 1로만 이루어져 있으며, 0은 벽이 있는 자리, 1은 벽
 from collections import deque
 
 def solution(maps):
-    # n * m
-    m = len(maps)
-    n = len(maps[0])
-    visited = [[0]*n for i in range(m)]
-
-    dx = [0,0,-1,1]
-    dy = [-1,1,0,0]
+    answer = 0
+    dx = [-1,1,0,0]
+    dy = [0,0,-1,1]
 
     q = deque()
-    q.append((0,0))
+    q.append((0,0))		# 0,0에서 시작
 
     while q:
         x, y = q.popleft()
-        
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if 0<=nx<m and 0<=ny<n and maps[nx][ny] == 1:
-                maps[nx][ny] = maps[x][y] + 1
-                q.append((nx, ny))
+            N = len(maps)
+            M = len(maps[0])
 
-    if maps[n-1][m-1] == 1:
-        return -1
-    else:
-        return maps[n-1][m-1]
+            if 0<=nx<N and 0<=ny<M and maps[nx][ny]==1:
+                maps[nx][ny] = maps[x][y]+1
+                q.append((nx,ny))
+    answer = maps[N-1][M-1]
     
+    if answer ==1:
+        answer = -1
+
+    return answer
 
 print(solution([[1,0,1,1,1],[1,0,1,0,1],[1,0,1,1,1],[1,1,1,0,1],[0,0,0,0,1]]))
