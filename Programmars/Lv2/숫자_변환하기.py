6fa9e3542ee0bd51701e45a6fb27395a26527af9
6uohul <kiy010604@naver.com>
2025-03-24 17:42:45 +0900

    [Python] 숫자변환하기 BFS

--- Programmars/Lv2/숫자_변환하기.py
+++ Programmars/Lv2/숫자_변환하기.py
@@ -33,3 +33,27 @@ def solution(x, y, n):
     return m[y]
 
 print(solution(2,5,4))
+
+
+################################################
+
+# BFS로 풀어보기
+
+from collections import deque
+ 
+def solution(x, y, n):
+    visited = [0] * 1000001
+ 
+    q = deque()
+    q.append((x, 0))
+    visited[x] = 1
+    while q:
+        num, cnt = q.popleft()
+        if num == y:
+            return cnt
+        for next_num in (num + n, num * 2, num * 3):
+            if next_num <= y and not visited[next_num]:
+                visited[next_num] = 1
+                q.append((next_num, cnt + 1))
+ 
+    return -1
