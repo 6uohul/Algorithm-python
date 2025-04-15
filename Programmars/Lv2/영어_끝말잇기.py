dc702db307078dd789f3a1a36a863dd78b76f03a
6uohul <kiy010604@naver.com>
2025-04-15 12:52:15 +0900

    [Python] 영어 끝말잇기

---
+++ Programmars/Lv2/영어_끝말잇기.py
@@ -0,0 +1,29 @@
+'''
+사람의 수 n과 사람들이 순서대로 말한 단어 words 가 매개변수로 주어질 때, 
+가장 먼저 탈락하는 사람의 번호와 그 사람이 자신의 몇 번째 차례에 탈락하는지를 구해서 return 하도록 solution 함수를 완성해주세요.
+
+제한 사항
+끝말잇기에 참여하는 사람의 수 n은 2 이상 10 이하의 자연수입니다.
+words는 끝말잇기에 사용한 단어들이 순서대로 들어있는 배열이며, 길이는 n 이상 100 이하입니다.
+단어의 길이는 2 이상 50 이하입니다.
+모든 단어는 알파벳 소문자로만 이루어져 있습니다.
+끝말잇기에 사용되는 단어의 뜻(의미)은 신경 쓰지 않으셔도 됩니다.
+정답은 [ 번호, 차례 ] 형태로 return 해주세요.
+만약 주어진 단어들로 탈락자가 생기지 않는다면, [0, 0]을 return 해주세요.
+'''
+
+def solution(n, words):
+    checked = [words[0]]
+    for i in range (1, len(words)):
+        if words[i][0] == words[i-1][-1] and words[i] not in checked:
+            checked.append(words[i])
+        else:
+            print([i%n, i//n])
+            return [i%n + 1, i//n + 1]
+    
+    print([0, 0])
+    return [0, 0]
+
+solution(3, ["tank", "kick", "know", "wheel", "land", "dream", "mother", "robot", "tank"])
+solution(5, ["hello", "observe", "effect", "take", "either", "recognize", "encourage", "ensure", "establish", "hang", "gather", "refer", "reference", "estimate", "executive"])
+solution(2, ["hello", "one", "even", "never", "now", "world", "draw"])
