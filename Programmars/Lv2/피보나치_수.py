1d8f9724d26c7501acb5e3a172f31ee899dc7803
6uohul <kiy010604@naver.com>
2025-04-18 09:46:05 +0900

    [Python] 피보나치 수 dp

--- Programmars/Lv2/피보나치_수.py
+++ Programmars/Lv2/피보나치_수.py
@@ -6,12 +6,10 @@ n은 2 이상 100,000 이하인 자연수입니다.
 '''
 
 def solution(n):
-    answer = 0
-    if n == 0:
-        return 0
-    if n == 1:
-        return 1
-    else:
-        answer = solution(n-2) + solution(n-1)
-    
-    print(answer % 1234567)
+    dp = [0, 1]
+    for i in range(2, n+1):
+        dp.append(dp[i-2] + dp[i-1])
+    return dp[-1] % 1234567
+
+
+print(solution(5))
